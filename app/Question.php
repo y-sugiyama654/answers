8a57fb3027212626f30ec721bc2fccff42e0f1f6
Yuta <y.sugiyama54@gmail.com>
2020-06-07 16:21:04 +0900

    questionのカウンター機能とビューの実装

--- app/Question.php
+++ app/Question.php
@@ -49,4 +49,20 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    /**
+     * questionのステータスを状況事に取得
+     *
+     * @return string ステータス
+     */
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return 'answered-accepted';
+            }
+            return 'answered';
+        }
+        return 'unanswered';
+    }
 }

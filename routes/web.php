c640e776fb1d7454eac53c6162cd32cf78d2596e
Yuta <y.sugiyama54@gmail.com>
2020-06-11 19:48:15 +0900

    answer編集機能の実装

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('questions', 'QuestionsController')->except('show');
 Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::get('/questions/{question}/answers/{answer}/edit', 'AnswersController@edit')->name('answers.edit');
-Route::put('/questions/{question}/answers/{answer}', 'AnswersController@update')->name('answers.update');
+Route::patch('/questions/{question}/answers/{answer}', 'AnswersController@update')->name('answers.update');
 Route::delete('/questions/{question}/answers/{answer}', 'AnswersController@destroy')->name('answers.destroy');
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');

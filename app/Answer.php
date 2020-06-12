d8a7dc1e667db3be254fa0501427643f60689a6e
Yuta <y.sugiyama54@gmail.com>
2020-06-13 03:23:28 +0900

    ベストアンサー選出時の認可処理を実装

--- app/Answer.php
+++ app/Answer.php
@@ -72,6 +72,24 @@ class Answer extends Model
      */
     public function getStatusAttribute()
     {
-        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+        return $this->isBest() ? 'vote-accepted' : '';
+    }
+
+    /**
+     * ベストアンサーが選出されているか true:選出済み false:未選出
+     */
+    public function getIsBestAttribute()
+    {
+        return $this->isBest();
+    }
+
+    /**
+     * answerがquestionのベストアンサーか
+     *
+     * @return bool
+     */
+    public function isBest()
+    {
+        return $this->id === $this->question->best_answer_id;
     }
 }
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -17,15 +17,25 @@
                             <a title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark this answer as best answer"
-                               class="{{ $answer->status }} mt-3"
-                               onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
-                            >
-                                <i class="fas fa-check fa-2x"></i>
-                            </a>
-                            <form action="{{ route('answers.accept', $answer->id) }}" id="accept-answer-{{ $answer->id }}" method="POST" style="display:none;">
-                                @csrf
-                            </form>
+                            @can ('accept', $answer)
+                                <a title="Mark this answer as best answer"
+                                   class="{{ $answer->status }} mt-3"
+                                   onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
+                                >
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+                                <form action="{{ route('answers.accept', $answer->id) }}" id="accept-answer-{{ $answer->id }}" method="POST" style="display:none;">
+                                    @csrf
+                                </form>
+                            @else
+                                @if ($answer->is_best)
+                                    <a title="The question owner accepted"
+                                       class="{{ $answer->status }} mt-3"
+                                    >
+                                        <i class="fas fa-check fa-2x"></i>
+                                    </a>
+                                @endif
+                            @endcan
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}

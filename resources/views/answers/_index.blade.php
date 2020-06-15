a6b694543f1c134a0c95010e0e1c919eeafbd019
Yuta <y.sugiyama54@gmail.com>
2020-06-16 02:07:59 +0900

    QuestionビューとAnswerビューへのDRY原則適用

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -9,48 +9,9 @@
                 @include('layouts._messages')
                 @foreach ($answers as $answer)
                     <div class="media">
-                        <div class="d-flex flex-column vote-controls">
-                            <a title="This answer is useful"
-                               class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                               onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit();"
-                            >
-                                <i class="fas fa-caret-up fa-3x"></i>
-                            </a>
-                            <form action="/answers/{{ $answer->id }}/vote" id="up-vote-answer-{{ $answer->id }}" method="POST" style="display:none;">
-                                @csrf
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-                            <span class="votes-count">{{ $answer->votes_count }}</span>
-                            <a title="This question is not useful"
-                               class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                               onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit();"
-                            >
-                                <i class="fas fa-caret-down fa-3x"></i>
-                            </a>
-                            <form action="/answers/{{ $answer->id }}/vote" id="down-vote-answer-{{ $answer->id }}" method="POST" style="display:none;">
-                                @csrf
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-                            @can ('accept', $answer)
-                                <a title="Mark this answer as best answer"
-                                   class="{{ $answer->status }} mt-3"
-                                   onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
-                                >
-                                    <i class="fas fa-check fa-2x"></i>
-                                </a>
-                                <form action="{{ route('answers.accept', $answer->id) }}" id="accept-answer-{{ $answer->id }}" method="POST" style="display:none;">
-                                    @csrf
-                                </form>
-                            @else
-                                @if ($answer->is_best)
-                                    <a title="The question owner accepted"
-                                       class="{{ $answer->status }} mt-3"
-                                    >
-                                        <i class="fas fa-check fa-2x"></i>
-                                    </a>
-                                @endif
-                            @endcan
-                        </div>
+                        @include ('shared._vote', [
+                            'model' => $answer,
+                        ])
                         <div class="media-body">
                             {!! $answer->body_html !!}
                             <div class="row">
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -16,42 +16,9 @@
                         </div>
                         <hr>
                         <div class="media">
-                            <div class="d-flex flex-column vote-controls">
-                                <a title="This question is useful"
-                                   class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                                   onclick="event.preventDefault(); document.getElementById('up-vote-question-{{ $question->id }}').submit();"
-                                >
-                                    <i class="fas fa-caret-up fa-3x"></i>
-                                </a>
-                                <form action="/questions/{{ $question->id }}/vote" id="up-vote-question-{{ $question->id }}" method="POST" style="display:none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="1">
-                                </form>
-                                <span class="votes-count">{{ $question->votes_count }}</span>
-                                <a title="This question is not useful"
-                                   class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                                   onclick="event.preventDefault(); document.getElementById('down-vote-question-{{ $question->id }}').submit();"
-                                >
-                                    <i class="fas fa-caret-down fa-3x"></i>
-                                </a>
-                                <form action="/questions/{{ $question->id }}/vote" id="down-vote-question-{{ $question->id }}" method="POST" style="display:none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="-1">
-                                </form>
-                                <a title="Click to mark as favorite question (Click again to undo)"
-                                    class="favorite mt-3 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
-                                    onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
-                                >
-                                    <i class="fas fa-star fa-2x"></i>
-                                    <span class="favorites-count">{{ $question->favorites_count }}</span>
-                                </a>
-                                <form action="/questions/{{ $question->id }}/favorites" id="favorite-question-{{ $question->id }}" method="POST" style="display:none;">
-                                    @csrf
-                                    @if ($question->is_favorited)
-                                        @method('DELETE')
-                                    @endif
-                                </form>
-                            </div>
+                            @include ('shared._vote', [
+                                'model' => $question,
+                            ])
                             <div class="media-body">
                                 {!! $question->body_html !!}
                                 <div class="row">

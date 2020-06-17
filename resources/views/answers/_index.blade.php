2a721795ee2fdb55b96577da79367089e67a8673
Yuta <y.sugiyama54@gmail.com>
2020-06-17 23:22:52 +0900

    questionとanswerを各投稿をpartialに分離

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -9,41 +9,7 @@
                     <hr>
                     @include('layouts._messages')
                     @foreach ($answers as $answer)
-                        <div class="media">
-                            @include ('shared._vote', [
-                                'model' => $answer,
-                            ])
-                            <div class="media-body">
-                                {!! $answer->body_html !!}
-                                <div class="row">
-                                    <div class="col-4">
-                                        <div class="ml-auto">
-                                            @can ('update', $answer)
-                                                <a href="{{ route('answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                            @endcan
-                                            @can ('delete', $answer)
-                                                <form class="form-delete" action="{{ route('answers.destroy', [$question->id, $answer->id]) }}" method="post">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </div>
-                                    <div class="col-4">
-
-                                    </div>
-                                    <div class="col-4">
-                                        @include ('shared._author', [
-                                            'model' => $answer,
-                                            'label' => 'answered',
-                                        ])
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <hr>
+                        @include ('answers._answer', ['answer' => $answer])
                     @endforeach
                 </div>
             </div>

d16c887c9e2cc4cafb88f30d895ec8e3bd3e6694
Yuta <y.sugiyama54@gmail.com>
2020-06-23 22:57:13 +0900

    acceptボタンをVue.jsのコンポーネントでリプレイス

---
+++ resources/js/components/Accept.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <a v-if="canAccept" title="Mark this answer as best answer" :class="classes">
+            <i class="fas fa-check fa-2x"></i>
+        </a>
+        <a v-if="accepted" title="The question owner accepted" :class="classes">
+            <i class="fas fa-check fa-2x"></i>
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['answer'],
+
+        data () {
+            return {
+                isBest: this.answer.is_best
+            }
+        },
+
+        computed: {
+            canAccept () {
+                return true;
+            },
+
+            accepted () {
+                return !this.canAccept && this.answer.is_best;
+            },
+
+            classes () {
+                return [
+                    'mt-2',
+                    this.accepted ? 'vote-accepted' : ''
+                ]
+            }
+        }
+    }
+</script>

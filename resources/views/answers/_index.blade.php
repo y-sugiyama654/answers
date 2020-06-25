539056a0e70d3eb210afab4e179d8fa5fdcb6af0
Yuta <y.sugiyama54@gmail.com>
2020-06-25 23:34:55 +0900

    answerをVue.jsのコンポーネントでリプレイス

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -1,19 +1,4 @@
 @if ($answersCount > 0)
-    <div class="row mt-4" v-cloak>
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-title">
-                        <h2>{{ $answersCount . ' ' . Str::plural('Answer', $answersCount) }}</h2>
-                    </div>
-                    <hr>
-                    @include('layouts._messages')
-                    @foreach ($answers as $answer)
-                        @include ('answers._answer', ['answer' => $answer])
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
+
 @endif
 

fffe11b9d48cbf3bff24899233e7ead510f5cbd8
Yuta <y.sugiyama54@gmail.com>
2020-07-07 00:40:56 +0900

    Question一覧ページのAPIエンドポイントの実装

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/token', 'Auth\LoginController@getToken');
+Route::get('/questions', 'Api\QuestionsController@index');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();

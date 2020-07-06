2072e7d3ce3752e48238d419d6de13aaadff2a42
Yuta <y.sugiyama54@gmail.com>
2020-07-06 23:31:06 +0900

    API認証の設定

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function getToken(Request $request)
+    {
+        $request->request->add([
+            'grant_type'    => 'password',
+            'client_id'     => 2,
+            'client_secret' => 'zBVuWXRZGZlvutu52htUO7wmhg0Q6rJdYH0vncyn',
+            'username'      => $request->username,
+            'password'      => $request->password,
+        ]);
+
+        $requestToken = Request::create(env('APP_URL') . '/oauth/token', 'post');
+        $response = Route::dispatch($requestToken);
+
+        return $response;
     }
 }

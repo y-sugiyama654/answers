facd659ef05ce8404489c495f61d86f1c7561069
Yuta <y.sugiyama54@gmail.com>
2020-07-30 22:25:09 +0900

    ログインエンドポイントの修正

--- public/js/app.js
+++ public/js/app.js
@@ -71586,6 +71586,7 @@ try {
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.baseURL = window.Urls.api || 'http://localhost/api';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
@@ -72619,7 +72620,7 @@ router.beforeEach(function (to, from, next) {
   if (to.matched.some(function (r) {
     return r.meta.requiresAuth;
   })) {
-    window.location = window.Auth.url;
+    window.location = window.Urls.login;
     return;
   }
 

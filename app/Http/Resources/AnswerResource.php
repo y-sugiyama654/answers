fcf48a271f027c155d7cf69d7a515756046a7fea
Yuta <y.sugiyama54@gmail.com>
2020-08-09 07:02:44 +0900

    Answer詳細表示処理の実装(SPA化)

--- app/Http/Resources/AnswerResource.php
+++ app/Http/Resources/AnswerResource.php
@@ -15,14 +15,15 @@ class AnswerResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'              => $this->id,
-            'votes_count'     => $this->votes_count,
-            'is_best'         => $this->is_best,
-            'body'            => $this->body,
-            'body_html'       => $this->body_html,
-            'user'            => new UserResource($this->user),
-            'question_id'     => $this->question_id,
-            'created_date'    => $this->created_date,
+            'id'               => $this->id,
+            'votes_count'      => $this->votes_count,
+            'is_best'          => $this->is_best,
+            'body'             => $this->body,
+            'body_html'        => $this->body_html,
+            'user'             => new UserResource($this->user),
+            'question_id'      => $this->question_id,
+            'created_date'     => $this->created_date,
+            'question_user_id' => $this->question->user_id
         ];
     }
 }

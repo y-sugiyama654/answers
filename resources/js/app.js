bef30e5426ad8d21cf217e4673c7b9fdfb910a18
Yuta <y.sugiyama54@gmail.com>
2020-08-22 15:53:49 +0900

    ローディング時にspinnerアイコンを表示

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,7 @@ import 'izitoast/dist/css/iziToast.css';
 import 'izitoast/dist/css/iziToast.min.css';
 import Authorization from './authorization/authorize';
 import router from './router';
+import Spinner from './components/Spinner.vue';
 
 Vue.use(VueIziToast);
 Vue.use(Authorization);
@@ -27,7 +28,7 @@ Vue.use(Authorization);
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-Vue.component('question-page', require('./pages/QuestionPage.vue').default);
+Vue.component('spinner', Spinner);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -37,5 +38,30 @@ Vue.component('question-page', require('./pages/QuestionPage.vue').default);
 
 const app = new Vue({
     el: '#app',
+
+    data: {
+      loading: false
+    },
+
+    created () {
+        // Add a request interceptor
+        axios.interceptors.request.use((config) => {
+            this.loading = true;
+            return config;
+        }, (error) => {
+            this.loading = false;
+            return Promise.reject(error);
+        });
+
+        // Add a response interceptor
+        axios.interceptors.response.use((response) => {
+            this.loading = false;
+            return response;
+        }, (error) => {
+            this.loading = false;
+            return Promise.reject(error);
+        });
+    },
+
     router
 });
